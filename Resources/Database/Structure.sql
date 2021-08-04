3463b1bd04fa50569ca9730ad838145a915422a1
Eduardo Bessa <=>
2021-08-04 16:39:03 +0100

    Update file sql

--- Resources/Database/Structure.sql
+++ Resources/Database/Structure.sql
@@ -1,5 +1,11 @@
 create database if not exists datadev;
 
+use datadev;
+
+drop table if exists postcodes;
+drop table if exists districts;
+drop table if exists counties;
+
 create table postcodes (
     id bigint unsigned auto_increment primary key,
     district varchar(30) null,
@@ -11,6 +17,20 @@ create table postcodes (
 
 create table districts (
     id bigint unsigned auto_increment primary key,
-    district varchar(30) unique not null,
+    postcode varchar(10) unique not null,
+    district varchar(30) not null,
+    counties int default 0,
+    parish int default 0,
     scrapped tinyint default 0
-)
+);
+
+create table counties (
+    id bigint unsigned auto_increment primary key,
+    district_id bigint unsigned not null,
+    county varchar(50) not null,
+    parish int default 0,
+    scrapped tinyint default 0
+);
+
+select * from districts;
+select * from counties;
